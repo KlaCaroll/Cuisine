923de5a60be7f4be608cf007d0102cda9452b54e
caroll kla <caroll.kla@gmail.com>
2022-05-24 17:28:10 +0200

    24 mai 17h05

---
+++ data/foodt.sql
@@ -0,0 +1,17 @@
+INSERT INTO food (id, name ) VALUES (18, 'riz');
+INSERT INTO food (id, name ) VALUES (2, 'oeuf');
+INSERT INTO food (id, name ) VALUES (3, 'farine');
+INSERT INTO food (id, name ) VALUES (4, 'creme fraich');
+INSERT INTO food (id, name ) VALUES (5, 'lardon');
+INSERT INTO food (id, name ) VALUES (6, 'pain de mie');
+INSERT INTO food (id, name ) VALUES (7, 'jambon');
+INSERT INTO food (id, name ) VALUES (8, 'parmesan');
+INSERT INTO food (id, name ) VALUES (9, 'chorizo');
+INSERT INTO food (id, name ) VALUES (10, 'gruyere');
+INSERT INTO food (id, name ) VALUES (11, 'lait');
+INSERT INTO food (id, name ) VALUES (12, 'oignon');
+INSERT INTO food (id, name ) VALUES (13, 'saumon');
+INSERT INTO food (id, name ) VALUES (14, 'petit poid');
+INSERT INTO food (id, name ) VALUES (15, 'pate brisee');
+INSERT INTO food (id, name ) VALUES (16, 'patate');
+INSERT INTO food (id, name ) VALUES (17, 'tomate');
