87986e7e6196033eaf33738620a7ed623b8eafdc
caroll kla <caroll.kla@gmail.com>
2022-05-25 11:18:01 +0200

    25 mai 11h17

--- data/foodt.sql
+++ data/foodt.sql
@@ -1,17 +1,18 @@
-INSERT INTO food (id, name ) VALUES (18, 'riz');
-INSERT INTO food (id, name ) VALUES (2, 'oeuf');
-INSERT INTO food (id, name ) VALUES (3, 'farine');
-INSERT INTO food (id, name ) VALUES (4, 'creme fraich');
-INSERT INTO food (id, name ) VALUES (5, 'lardon');
-INSERT INTO food (id, name ) VALUES (6, 'pain de mie');
-INSERT INTO food (id, name ) VALUES (7, 'jambon');
-INSERT INTO food (id, name ) VALUES (8, 'parmesan');
-INSERT INTO food (id, name ) VALUES (9, 'chorizo');
-INSERT INTO food (id, name ) VALUES (10, 'gruyere');
-INSERT INTO food (id, name ) VALUES (11, 'lait');
-INSERT INTO food (id, name ) VALUES (12, 'oignon');
-INSERT INTO food (id, name ) VALUES (13, 'saumon');
-INSERT INTO food (id, name ) VALUES (14, 'petit poid');
-INSERT INTO food (id, name ) VALUES (15, 'pate brisee');
-INSERT INTO food (id, name ) VALUES (16, 'patate');
-INSERT INTO food (id, name ) VALUES (17, 'tomate');
+INSERT INTO food (id, name ) VALUES 
+    (18, 'riz'),
+    (2, 'oeuf'),
+    (3, 'farine'),
+    (4, 'creme fraich'),
+    (5, 'lardon'),
+    (6, 'pain de mie'),
+    (7, 'jambon'),
+    (8, 'parmesan'),
+    (9, 'chorizo'),
+    (10, 'gruyere'),
+    (11, 'lait'),
+    (12, 'oignon'),
+    (13, 'saumon'),
+    (14, 'petit poid'),
+    (15, 'pate brisee'),
+    (16, 'patate'),
+    (17, 'tomate');
