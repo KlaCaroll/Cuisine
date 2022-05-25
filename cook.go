7de5e81d536e9ea0b5bf0b20a554c22904844e0a
caroll kla <caroll.kla@gmail.com>
2022-05-25 12:56:07 +0200

    25 mai 12h55

--- cook.go
+++ cook.go
@@ -2,8 +2,11 @@ package main
 
 import (
 //	"fmt"
+	"database/sql"
+	_"github.com/mattn/go-sqlite3" 
 	"log"
 	"net/http"
+	"os"
 )
 
 func fetch(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +14,20 @@ func fetch(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	os.Remove("data/database.db")
+
+	db, err := sql.Open("sqlite3", "data/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT food.name as ingredients, quantity from recipe_food, food where food_id = food.id and recipe_id = 121191714519;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
 	http.HandleFunc("/connect", fetch)
 
 
